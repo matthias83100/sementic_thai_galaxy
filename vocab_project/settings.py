a8c9f12ee8d7737f4c7f1a079e3c19ad6ee05777
Matthias STOLFI <matthias.sto@roojai.com>
2026-02-18 17:36:36 +0700

    Security update

--- vocab_project/settings.py
+++ vocab_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dmq3o1w+4sslts1_+_fk@&%c#kdg8-(z$-(8e*mc09rfc!5#q3')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
